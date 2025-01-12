fdb8d2193409cdd05932f9130066bd5d5b697885
nsiva2923 <nsiva2923@gmail.com>
2025-01-12 12:57:17 +0530

    first program

--- src/main/java/org/example/FirstProgram.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class FirstProgram {
-    public static void main(String[] args) {
-        System.out.println("Welcome to NSKRCoding");
-    }
-}
---
+++ src/main/java/org/nskr/FirstProgram.java
@@ -0,0 +1,12 @@
+package org.nskr;
+
+import java.util.Arrays;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.print("Welcome");
+        System.out.print("NSKR");
+        System.out.println("Coding");
+        System.out.println("Welcome NSKR Coding");
+    }
+}
